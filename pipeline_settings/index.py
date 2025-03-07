c93aeba294b893ffdea64d746f8e8ecd0f169662
Ko-193 <68505470+Ko-193@users.noreply.github.com>
2025-03-07 16:02:08 +0900

    Update index.py

--- pipeline_settings/index.py
+++ pipeline_settings/index.py
@@ -5,6 +5,5 @@ def lambda_handler(event, context):
     print(event)
     return {
         'statusCode': 200,
-        #'body': json.dumps('Hello Lambda!')
-        'body': json.dumps('Lambda!')
+        'body': json.dumps('Goodbye Lambda!')
     }
